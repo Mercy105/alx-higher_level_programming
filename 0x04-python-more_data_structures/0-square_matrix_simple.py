cf9686e61ec6599b9607bb73e74424503282b100
Mercy105 <wawerumercy105@gmail.com>
2023-09-15 12:13:50 -0700

    multiply

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for ind_a in matrix:
-        new_matrix[len(new_matrix):] = [[elem ** 2 for elem in ind_a]]
-    return new_matrix
+def multiply_by_2(a_dictionary):
+    return {k: a_dictionary[k] * 2 for k in a_dictionary}
