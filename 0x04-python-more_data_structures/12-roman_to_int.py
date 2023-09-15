14020416b4ed9143cf58184f71279d538c19a3e4
Mercy105 <wawerumercy105@gmail.com>
2023-09-15 12:32:03 -0700

    test1

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,4 +10,4 @@ def roman_to_int(roman_string: str):
             rep += numbers[i]
         else:
             rep -= numbers[i]
-    return rep                                                      
+    return rep
