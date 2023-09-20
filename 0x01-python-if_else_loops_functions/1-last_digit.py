7c36da08873903be12ab3400783d57f9a4ddc100
Mercy105 <wawerumercy105@gmail.com>
2023-09-20 01:58:27 -0700

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import random
-	number = random.randint(-10000, 10000)
-	lastDigit = number % 10 if number > 0 else int(repr(number)[-1]) * -1
-		print("Last digit of {} is {} ".format(number, lastDigit), end="")
-	if lastDigit > 5:
-		print("and is greater than 5")
-	elif lastDigit == 0:
-		print("and is 0")
-	else:
-		print("and is less than 6 and not 0")
+number = random.randint(-10000, 10000)
+last = abs(number) % 10
+    if number < 0:
+        last *= -1
+    if last > 5:
+        print("Last digit of {:d} is {:d} and is greater than 5"
+        .format(number, int(last)))
+    elif last == 0:
+        print("Last digit of {:d} is {:d} and is 0".format(number, int(last)))
+    else:
+        print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+                .format(number, int(last)))
